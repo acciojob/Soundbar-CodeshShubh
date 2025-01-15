273b8f734b28c2f533646bf9341fc7684ea1299f
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-15 18:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,19 @@
-// Global variable to hold the currently playing audio
-let currentAudio = null;
-
-let playBtns = document.querySelectorAll('.btn'); // Fixed typo: querySelectorAll
-
-playBtns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        // Stop the previous sound if it's playing
-        if (currentAudio) {
-            currentAudio.pause();
-            currentAudio.currentTime = 0; // Reset to start
-        }
-        
-        // Play the new sound based on button clicked
-        switch (e.target.innerText) {
-            case 'applause':
-                currentAudio = new Audio('sounds/applause.mp3');
-                currentAudio.play();
-                break;
-            case 'boo':
-                currentAudio = new Audio('sounds/boo.mp3');
-                currentAudio.play();
-                break;
-            case 'gasp':
-                currentAudio = new Audio('sounds/gasp.mp3');
-                currentAudio.play();
-                break;
-            case 'tada':
-                currentAudio = new Audio('sounds/tada.mp3');
-                currentAudio.play();
-                break;
-            case 'victory':
-                currentAudio = new Audio('sounds/victory.mp3');
-                currentAudio.play();
-                break;
-            case 'wrong':
-                currentAudio = new Audio('sounds/wrong.mp3');
-                currentAudio.play();
-                break;
-            default:
-                console.log("Unknown action");
-                break;
-        }
-    });
-});
-
-// Stop button to pause the audio
-const stopButton = document.querySelector('.stop');
-stopButton.addEventListener('click', () => {
-    if (currentAudio) {
-        currentAudio.pause();  // Pause the currently playing audio
-        currentAudio.currentTime = 0; // Reset to the start of the audio
+function soundPlay(audioPlayer){ 
+let audioPlayer = document.getElementsByClassName("btn");
+audioPlayer.src = "D:\New folder\sound\short-crowd-cheer-6713.mp3"+ soundbar;
+audioPlayer.play();
+}
+btns.addEventListener("click", soundPlay);
+function soundPlay(audioPlayer){ 
+    let audioPlayer = document.getElementsByClassName("btn");
+    audioPlayer.src = "D:\New folder\sound\boo-6377.mp3" + soundbar;
+    audioPlayer.play();
     }
-});
+    btns.addEventListener("click", soundPlay);
+    
+
+function soundStop(){
+    let audioPlayer = getElementsByClassName("stop");
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+}
